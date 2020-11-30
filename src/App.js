c14c0af6f167eea3d21c7e009f6276962af5a4c2
jonathancsr <jonathancsrr@gmail.com>
2020-11-30 15:14:25 -0300

    feat: add new architeture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import GlobalStyle from './assets/styles/global'
+import React from 'react';
+import GlobalStyle from './assets/styles/global';
+import { socket } from './service/web-socket';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
+  React.useEffect(() => {
+    socket.on('connect', client => console.log('conectei', client));
+  }, [socket]);
+
   return (
-    <>
-      <h1>Ola</h1>
-      <GlobalStyle/>
-    </>
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -6,6 +6,7 @@ export default createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
   outline: 0;
+  font-family: 'Roboto Slab', serif;
 }
 
 body {
@@ -15,12 +16,13 @@ body {
 }
 
 body, input, button {
-  font-family: 'Roboto Slab', serif;
   font-size:16px;
 }
 
 h1,h2,h3,h4,h5,h6,strong {
   font-weight:500;
+  color:#1d1d1d;
+  
 }
 
 button {
